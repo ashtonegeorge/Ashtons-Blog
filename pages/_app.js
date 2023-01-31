346101edf0876acccfd3fd9c38e244bf1d99e647
Ashton George <ashtongeorge17@gmail.com>
2023-01-30 20:48:06 -0500

    api http request fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Ashton George</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet" />
