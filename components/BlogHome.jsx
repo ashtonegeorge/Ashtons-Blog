7c5ca0cd7f141e473d366dbdddcb43119325c012
Ashton George <ashtongeorge17@gmail.com>
2023-03-06 11:35:29 -0500

    image fix attempt 2

--- components/BlogHome.jsx
+++ components/BlogHome.jsx
@@ -3,6 +3,9 @@ import { PostCard, Categories, PostWidget } from './BlogComponents';
 import { FeaturedPosts } from '../sections';
 
 export default function BlogHome({ posts }) {
+  const latestPosts = posts.map(item => item).reverse()
+
+
   return (
     <div className="container mx-auto px-10 mb-8" >
       <Head>
@@ -15,7 +18,7 @@ export default function BlogHome({ posts }) {
       <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
+          {latestPosts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
           
         <div className='lg:col-span-4 col-span-1'>
